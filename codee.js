99aea2f86a8374ce9b0c3b4ffbdc483b93858773
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-09-26 12:27:30 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,12 +1,23 @@
- {/* Search Fields */}
-                <Grid item xs={12} md={4}>
-                  <TextField
-                    fullWidth
-                    label="Search Employee ID"
-                    value={searchEmployeeId}
-                    onChange={(e) => setSearchEmployeeId(e.target.value)}
-                    size="small"
-                    helperText="Type an ID and click Run"
-                    sx={{ bgcolor: "#0000", borderRadius: 1, color: "#120101" }}
-                  />
-                </Grid>
+<Grid item xs={12} md={4}>
+  <TextField
+    fullWidth
+    label="Search Employee ID"
+    value={searchEmployeeId}
+    onChange={(e) => setSearchEmployeeId(e.target.value)}
+    size="small"
+    helperText="Type an ID and click Run"
+    sx={{
+      backgroundColor: "white",
+      borderRadius: 1,
+      "& .MuiInputBase-input": {
+        color: "black", // Text color
+      },
+      "& .MuiInputLabel-root": {
+        color: "black", // Label color
+      },
+      "& .MuiFormHelperText-root": {
+        color: "black", // Helper text color
+      },
+    }}
+  />
+</Grid>
