e27f340cad60511fb3c8484233e0b952ba9ee925
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-09-26 16:20:35 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,29 +1,38 @@
-
-
 <Grid item xs={12} md={3}>
-                      <TextField
-                        label="Start Date"
-                        type="date"
-                        fullWidth
-                        size="small"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                        inputRef={startDateRef}
-                        InputLabelProps={{ shrink: true }}
-                        InputProps={{
-                          sx: { color: "#000" },
-                          endAdornment: (
-                            <InputAdornment position="end">
-                              <IconButton
-                                size="small"
-                                onClick={() => openNativeDatePicker(startDateRef)}
-                                sx={{ color: "#e6c200" }}
-                              >
-                                <DateRangeIcon />
-                              </IconButton>
-                            </InputAdornment>
-                          ),
-                        }}
-                        sx={{ bgcolor: "#fff", borderRadius: 1, color: "#000" }}
-                      />
-                    </Grid>
+  <TextField
+    label="Start Date"
+    type="date"
+    fullWidth
+    size="small"
+    value={startDate}
+    onChange={(e) => setStartDate(e.target.value)}
+    inputRef={startDateRef}
+    InputLabelProps={{ shrink: true }}
+    InputProps={{
+      sx: { color: "#000" }, // text color inside input
+      endAdornment: (
+        <InputAdornment position="end">
+          <IconButton
+            size="small"
+            onClick={() => openNativeDatePicker(startDateRef)}
+            sx={{ color: "#e6c200" }}
+          >
+            <DateRangeIcon />
+          </IconButton>
+        </InputAdornment>
+      ),
+    }}
+    sx={{
+      bgcolor: "#fff",
+      borderRadius: 1,
+      color: "#000",
+      "& .MuiInputLabel-root": {
+        color: "#1976d2", // <-- label color
+        fontWeight: "bold",
+      },
+      "& .Mui-focused .MuiInputLabel-root": {
+        color: "#e6c200", // <-- label color when focused
+      },
+    }}
+  />
+</Grid>
