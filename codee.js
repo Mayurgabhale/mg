f7899d1556a20c4b9718d0fa2283b249050f8661
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-09-26 18:59:47 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,4 +1,18 @@
-chunk-PPVFY42G.js?v=a96f9ddc:21580 Download the React DevTools for a better development experience: https://reactjs.org/link/react-devtools
-chunk-SKHEGRPX.js?v=a96f9ddc:39 Uncaught TypeError: createTheme_default is not a function
-    at chunk-SKHEGRPX.js?v=a96f9ddc:39:20
-(anonymous) @ chunk-SKHEGRPX.js?v=a96f9ddc:39
+react-dom.development.js:29895 Download the React DevTools for a better development experience: https://reactjs.org/link/react-devtools
+deprecations.ts:9 ⚠️ React Router Future Flag Warning: React Router will begin wrapping state updates in `React.startTransition` in v7. You can use the `v7_startTransition` future flag to opt-in early. For more information, see https://reactrouter.com/v6/upgrading/future#v7_starttransition.
+warnOnce @ deprecations.ts:9
+:8000/ccure/stream:1  Failed to load resource: the server responded with a status of 404 (Not Found)
+GlobalPage.jsx:532 [SSE] error/closed, attempting reconnect Event
+es.onerror @ GlobalPage.jsx:532
+duration:1 Access to XMLHttpRequest at 'http://10.199.22.57:8001/duration/cities?region=apac' from origin 'http://10.199.22.57:3005' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+DurationPage.jsx:166 Failed to fetch cities list: Network Error
+fetchCities @ DurationPage.jsx:166
+:8001/duration/cities?region=apac:1  Failed to load resource: net::ERR_FAILED
+duration:1 Access to XMLHttpRequest at 'http://10.199.22.57:8001/duration?regions=apac' from origin 'http://10.199.22.57:3005' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+DurationPage.jsx:206 AxiosError
+(anonymous) @ DurationPage.jsx:206
+:8001/duration?regions=apac:1  Failed to load resource: net::ERR_FAILED
+duration:1 Access to XMLHttpRequest at 'http://10.199.22.57:8001/duration?date=2025-09-26&regions=apac' from origin 'http://10.199.22.57:3005' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+DurationPage.jsx:206 AxiosError
+(anonymous) @ DurationPage.jsx:206
+:8001/duration?date=2025-09-26&regions=apac:1  Failed to load resource: net::ERR_FAILED
