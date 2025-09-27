25eff7dbb59a58cdefe054fa0c3d00f9d2451f2a
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-09-27 10:59:12 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,4 +1,23 @@
 
+<TextField
+  fullWidth
+  size="small"
+  label="Reason (optional)"
+  value={overrideReason}
+  onChange={(e) => setOverrideReason(e.target.value)}
+  sx={{
+    "& .MuiInputBase-input": {
+      color: "black",   // text color
+    },
+    "& .MuiInputLabel-root": {
+      color: "black",   // label color
+    },
+    "& .MuiOutlinedInput-notchedOutline": {
+      borderColor: "black", // border color
+    },
+  }}
+/>
+....
           <Grid item xs={12}>
             <TextField
               fullWidth
