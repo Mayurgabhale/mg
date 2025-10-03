7636c3e99c7052535975f58ce622fbdc8a8f5b88
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-03 12:02:43 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,2 +1,34 @@
-import XLSX from "xlsx-js-style";
-import { saveAs } from "file-saver";
+add export to excle button, 
+<div className="table-responsive">
+
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <th>Sr</th>
+                                        {modalRows[0].company && <th>Company</th>}
+                                        <th>Name</th>
+                                        <th>Employee ID</th>
+                                        <th>Personnel Type</th>
+                                        <th>Zone</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+
+                                    {modalRows.map((r, i) => (
+                                        <tr key={`${r.employeeId || r.idx}-${i}`}>
+                                            <td>{r.idx}</td>
+                                            {r.company && <td>{r.company}</td>}
+                                            <td>{r.name}</td>
+                                            <td>{r.employeeId || "-"}</td>
+                                            <td>{r.personnelType || "-"}</td>
+                                            <td>{r.zone || "-"}</td>
+                                        </tr>
+                                    ))}
+
+
+                                </tbody>
+                            </Table>
+
+
+                        </div>
