007bbb92e5f98958716eb899e794d13a68d04960
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-03 18:41:19 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,9 +1,65 @@
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // STARTTLS
-    auth: {
-        user: "mayurgabhale709@gmail.com",
-        pass: "syhqmnzfmjmqsojh"
-    }
-});
+# Python code to illustrate Sending mail with attachments
+# from your Gmail account 
+
+# libraries to be imported
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+ 
+fromaddr = "mayurgabhale709@gmail.cm"
+toaddr = "codinmayur@gmail.com"
+ 
+# instance of MIMEMultipart
+msg = MIMEMultipart()
+
+# storing the senders email address  
+msg['From'] = fromaddr
+
+# storing the receivers email address 
+msg['To'] = toaddr
+
+# storing the subject 
+msg['Subject'] = "Subject of the Mail"
+
+# string to store the body of the mail
+body = "Body_of_the_mail"
+
+# attach the body with the msg instance
+msg.attach(MIMEText(body, 'plain'))
+
+
+
+
+# instance of MIMEBase and named as p
+p = MIMEBase('application', 'octet-stream')
+
+# To change the payload into encoded form
+
+
+# encode into base64
+encoders.encode_base64(p)
+ 
+
+
+# attach the instance 'p' to instance 'msg'
+msg.attach(p)
+
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login(fromaddr, "syhqmnzfmjmqsojh")
+
+# Converts the Multipart msg into a string
+text = msg.as_string()
+
+# sending the mail
+s.sendmail(fromaddr, toaddr, text)
+
+# terminating the session
+s.quit()
