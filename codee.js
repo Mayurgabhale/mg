59fd9311c6569fa303e54c8ce9b06fe677b8119e
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-03 18:23:09 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,1 +1,32 @@
-syhq mnzf mjmq sojh
+write correct code to send mail 
+const nodemailer = require('nodemailer')
+
+
+const transpoeter = nodemailer.createTransport(
+    {
+        secure:true,
+        host:'smtp.gmail.com',
+        port:465,
+        auth:{
+            user:'mayurgabhale709@gmail.com',
+            pass:'syhqmnzfmjmqsojh'
+        }
+    }
+);
+
+
+function sendMail(to,sub,msg){
+    transpoeter.sendMail({
+        to:to,
+        subject:sub,
+        html:msg
+    });
+
+    console.log("email sent succrfully, ")
+
+}
+
+sendMail("codingmayur@gmail.com", "this is a subject", "this is the message to send for mail ")
+
+
+PS C:\Users\W0024618\Desktop\swipeData\employee-ai-insights> node .\mailsend.js
