89b7dcebfecc1016c15c57f8d8f9e5717413c908
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-04 11:33:33 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,9 +1,77 @@
------------
-afte hove i want to dipsly name ok 
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+import { MdAddAlert } from "react-icons/md";
+import { FaBuildingCircleCheck } from "react-icons/fa6";
 
-             <Nav.Link as={Link} to="/" className="nav-item-infographic"><i className="bi bi-house"></i></Nav.Link> << HOMe
-            <Nav.Link as={Link} to="/details" className="nav-item-infographic"><i className="fa-solid fa-calendar-day"></i></Nav.Link> <<< Details page 
-            <Nav.Link as={Link} to="/ert" className="nav-item-infographic"> <MdAddAlert /> </Nav.Link> <<< ERT Overview
-            <Nav.Link as={Link} to="/companysummary" className="nav-item-infographic"><FaBuildingCircleCheck /></Nav.Link> << Company Summary
-            <Nav.Link href="http://10.199.22.57:3000/partition/Pune/history" className="nav-item-infographic"><i className="bi bi-clock-history"></i></Nav.Link> << History 
-            {/* // Also update the navbar link for Company Summary: */}
+function NavbarCustom() {
+  useEffect(() => {
+    // Initialize Bootstrap tooltips
+    const tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new window.bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }, []);
+
+  return (
+    <>
+      <Nav.Link 
+        as={Link} 
+        to="/" 
+        className="nav-item-infographic" 
+        data-bs-toggle="tooltip" 
+        data-bs-placement="bottom" 
+        title="Home"
+      >
+        <i className="bi bi-house"></i>
+      </Nav.Link>
+
+      <Nav.Link 
+        as={Link} 
+        to="/details" 
+        className="nav-item-infographic" 
+        data-bs-toggle="tooltip" 
+        data-bs-placement="bottom" 
+        title="Details Page"
+      >
+        <i className="fa-solid fa-calendar-day"></i>
+      </Nav.Link>
+
+      <Nav.Link 
+        as={Link} 
+        to="/ert" 
+        className="nav-item-infographic"
+        data-bs-toggle="tooltip"
+        data-bs-placement="bottom"
+        title="ERT Overview"
+      >
+        <MdAddAlert />
+      </Nav.Link>
+
+      <Nav.Link 
+        as={Link} 
+        to="/companysummary" 
+        className="nav-item-infographic"
+        data-bs-toggle="tooltip"
+        data-bs-placement="bottom"
+        title="Company Summary"
+      >
+        <FaBuildingCircleCheck />
+      </Nav.Link>
+
+      <Nav.Link 
+        href="http://10.199.22.57:3000/partition/Pune/history" 
+        className="nav-item-infographic"
+        data-bs-toggle="tooltip"
+        data-bs-placement="bottom"
+        title="History"
+      >
+        <i className="bi bi-clock-history"></i>
+      </Nav.Link>
+    </>
+  );
+}
+
+export default NavbarCustom;
