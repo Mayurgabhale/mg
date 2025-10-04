b78cb42477a010e99e1b044f32c06faa9df0498e
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-04 11:30:56 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,24 +1,9 @@
-# -*- coding: utf-8 -*-
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+-----------
+afte hove i want to dipsly name ok 
 
-fromaddr = "mayurgabhale709@gmail.com"
-toaddr = "codingmayur@gmail.com"
-
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "Subject of the Mail"
-
-body = "This is the body of the mail."
-msg.attach(MIMEText(body, 'plain'))
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, "syhqmnzfmjmqsojh")
-
-server.sendmail(fromaddr, toaddr, msg.as_string())
-server.quit()
-
-print("✅ Email sent successfully!")
+             <Nav.Link as={Link} to="/" className="nav-item-infographic"><i className="bi bi-house"></i></Nav.Link> << HOMe
+            <Nav.Link as={Link} to="/details" className="nav-item-infographic"><i className="fa-solid fa-calendar-day"></i></Nav.Link> <<< Details page 
+            <Nav.Link as={Link} to="/ert" className="nav-item-infographic"> <MdAddAlert /> </Nav.Link> <<< ERT Overview
+            <Nav.Link as={Link} to="/companysummary" className="nav-item-infographic"><FaBuildingCircleCheck /></Nav.Link> << Company Summary
+            <Nav.Link href="http://10.199.22.57:3000/partition/Pune/history" className="nav-item-infographic"><i className="bi bi-clock-history"></i></Nav.Link> << History 
+            {/* // Also update the navbar link for Company Summary: */}
