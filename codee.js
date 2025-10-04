3f96b619815ece68ddf0ad3e933ddedbe44d2d02
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-04 14:11:33 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,11 +1,9 @@
-const companyShortNames = {
-  "Western Union": "WU",
-  "Visitor": "Visitor",
-  "CLR Facility Services Pvt.Ltd.": "CLR",
-  "Poona Security India Pvt Ltd": "PSI",
-  "CBRE": "CBRE",
-  "Osource India Pvt Ltd": "Osource",
-  "Temp Badge": "Temp",
-  "Vedant Enterprises Pvt. Ltd": "Vedant",
-  "Tea Point": "Tea"
-};
+const chartData = useMemo(() => {
+  return (companyData?.companies || []).map((c) => ({
+    name: companyShortNames[c.name] || c.name,  // <-- short name
+    podium: c.byBuilding["Podium Floor"] || 0,
+    second: c.byBuilding["2nd Floor"] || 0,
+    towerB: c.byBuilding["Tower B"] || 0,
+    total: c.total || 0,
+  }));
+}, [companyData]);
