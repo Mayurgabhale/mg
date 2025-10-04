63691ac9a55d7e70cd1e2ea72fab5b0a261dcb30
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-04 11:41:59 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,77 +1,20 @@
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
-import { MdAddAlert } from "react-icons/md";
-import { FaBuildingCircleCheck } from "react-icons/fa6";
+ <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">Dashboard</Tooltip>}>
+                        <Nav.Link as={Link} to="/" className="nav-item-infographic"><i className="bi bi-house"></i></Nav.Link>
+</OverlayTrigger>
+   
+ <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">Live Detais Page</Tooltip>}>
+            <Nav.Link as={Link} to="/details" className="nav-item-infographic"><i className="fa-solid fa-calendar-day"></i></Nav.Link> 
+</OverlayTrigger>
+   
+ <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">ERT Overview</Tooltip>}>
+            <Nav.Link as={Link} to="/ert" className="nav-item-infographic"> <MdAddAlert /> </Nav.Link>
+</OverlayTrigger>
+   
+ <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">Company Summary</Tooltip>}>
+            <Nav.Link as={Link} to="/companysummary" className="nav-item-infographic"><FaBuildingCircleCheck /></Nav.Link>
+</OverlayTrigger>
+   
+ <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">History</Tooltip>}>
+                       <Nav.Link href="http://10.199.22.57:3000/partition/Pune/history" className="nav-item-infographic"><i className="bi bi-clock-history"></i></Nav.Link>
+</OverlayTrigger>
 
-function NavbarCustom() {
-  useEffect(() => {
-    // Initialize Bootstrap tooltips
-    const tooltipTriggerList = [].slice.call(
-      document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    );
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new window.bootstrap.Tooltip(tooltipTriggerEl);
-    });
-  }, []);
-
-  return (
-    <>
-      <Nav.Link 
-        as={Link} 
-        to="/" 
-        className="nav-item-infographic" 
-        data-bs-toggle="tooltip" 
-        data-bs-placement="bottom" 
-        title="Home"
-      >
-        <i className="bi bi-house"></i>
-      </Nav.Link>
-
-      <Nav.Link 
-        as={Link} 
-        to="/details" 
-        className="nav-item-infographic" 
-        data-bs-toggle="tooltip" 
-        data-bs-placement="bottom" 
-        title="Details Page"
-      >
-        <i className="fa-solid fa-calendar-day"></i>
-      </Nav.Link>
-
-      <Nav.Link 
-        as={Link} 
-        to="/ert" 
-        className="nav-item-infographic"
-        data-bs-toggle="tooltip"
-        data-bs-placement="bottom"
-        title="ERT Overview"
-      >
-        <MdAddAlert />
-      </Nav.Link>
-
-      <Nav.Link 
-        as={Link} 
-        to="/companysummary" 
-        className="nav-item-infographic"
-        data-bs-toggle="tooltip"
-        data-bs-placement="bottom"
-        title="Company Summary"
-      >
-        <FaBuildingCircleCheck />
-      </Nav.Link>
-
-      <Nav.Link 
-        href="http://10.199.22.57:3000/partition/Pune/history" 
-        className="nav-item-infographic"
-        data-bs-toggle="tooltip"
-        data-bs-placement="bottom"
-        title="History"
-      >
-        <i className="bi bi-clock-history"></i>
-      </Nav.Link>
-    </>
-  );
-}
-
-export default NavbarCustom;
