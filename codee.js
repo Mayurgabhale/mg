f0ac5027c2e02afb25dcaafe36a1b1765a1fba39
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-09 12:17:15 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,4 +1,10 @@
 
+
+
+
+
+npm install exceljs --save
+
 WARNING in ../node_modules/exceljs/dist/exceljs.min.js
 Module Warning (from ./node_modules/source-map-loader/dist/cjs.js):
 Failed to parse source map from 'C:\Users\W0024618\Desktop\namer-occupancy-frontend\node_modules\exceljs\dist\exceljs.min.js.map' file: Error: ENOENT: no such file or directory, open 'C:\Users\W0024618\Desktop\namer-occupancy-frontend\node_modules\exceljs\dist\exceljs.min.js.map'
