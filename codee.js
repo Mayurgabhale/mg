1da546f692d16bab67a9f58ccec4db242aca8047
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-17 16:28:37 +0530

    Update codee.jsjfgjfdgjfkgfjkgj

--- codee.js
+++ codee.js
@@ -3,7 +3,19 @@ http://localhost:3001/api/occupancy/history
     "success": false,
     "message": "Historical fetch failed"
 }
-equestError: Connection lost - A8580000:error:1C800066:Provider routines:ossl_gcm_stream_update:cipher operation failed:c:\ws\deps\openssl\openssl\providers\implementations\ciphers\ciphercommon_gcm.c:325:
+PS C:\Users\W0024618\desktop\laca-occupancy-backend> npm run dev
+
+> laca-occupancy-backend@1.0.0 dev
+> nodemon src/server.js
+
+[nodemon] 3.1.10
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching path(s): *.*
+[nodemon] watching extensions: js,mjs,cjs,json
+[nodemon] starting `node src/server.js`
+🚀 Server running on port 3001
+✅ MSSQL connected
+RequestError: Connection lost - 706E0000:error:1C800066:Provider routines:ossl_gcm_stream_update:cipher operation failed:c:\ws\deps\openssl\openssl\providers\implementations\ciphers\ciphercommon_gcm.c:325:
 
     at handleError (C:\Users\W0024618\Desktop\laca-occupancy-backend\node_modules\mssql\lib\tedious\request.js:384:15)
     at Connection.emit (node:events:530:35)
@@ -15,7 +27,7 @@ equestError: Connection lost - A8580000:error:1C800066:Provider routines:ossl_gc
     at emitErrorCloseNT (node:internal/streams/destroy:129:3)
     at process.processTicksAndRejections (node:internal/process/task_queues:90:21) {
   code: 'EREQUEST',
-  originalError: Error: Connection lost - A8580000:error:1C800066:Provider routines:ossl_gcm_stream_update:cipher operation failed:c:\ws\deps\openssl\openssl\providers\implementations\ciphers\ciphercommon_gcm.c:325:
+  originalError: Error: Connection lost - 706E0000:error:1C800066:Provider routines:ossl_gcm_stream_update:cipher operation failed:c:\ws\deps\openssl\openssl\providers\implementations\ciphers\ciphercommon_gcm.c:325:
 
       at handleError (C:\Users\W0024618\Desktop\laca-occupancy-backend\node_modules\mssql\lib\tedious\request.js:382:19)
       at Connection.emit (node:events:530:35)
@@ -26,7 +38,7 @@ equestError: Connection lost - A8580000:error:1C800066:Provider routines:ossl_gc
       at emitErrorNT (node:internal/streams/destroy:170:8)
       at emitErrorCloseNT (node:internal/streams/destroy:129:3)
       at process.processTicksAndRejections (node:internal/process/task_queues:90:21) {
-    info: ConnectionError: Connection lost - A8580000:error:1C800066:Provider routines:ossl_gcm_stream_update:cipher operation failed:c:\ws\deps\openssl\openssl\providers\implementations\ciphers\ciphercommon_gcm.c:325:
+    info: ConnectionError: Connection lost - 706E0000:error:1C800066:Provider routines:ossl_gcm_stream_update:cipher operation failed:c:\ws\deps\openssl\openssl\providers\implementations\ciphers\ciphercommon_gcm.c:325:
 
         at Connection.socketError (C:\Users\W0024618\Desktop\laca-occupancy-backend\node_modules\tedious\lib\connection.js:1359:26)
         at Socket.<anonymous> (C:\Users\W0024618\Desktop\laca-occupancy-backend\node_modules\tedious\lib\connection.js:1060:12)
@@ -45,8 +57,6 @@ equestError: Connection lost - A8580000:error:1C800066:Provider routines:ossl_gc
   serverName: undefined,
   procName: undefined
 }
-
-
 
 
 //Abhishek//1//
