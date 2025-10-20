369ff6618fdb85a8c727d953281cbf4494bd05e4
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-20 10:17:12 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -55,3 +55,9 @@ Historical fetch failed: RequestError: Connection lost - read ECONNRESET
   class: undefined,
   serverName: undefined,
   procName: undefined
+------------------------------------
+  Uncaught runtime errors:
+×
+ERROR
+History fetch failed: 500
+    at fetchHistory (http://localhost:3000/static/js/src_components_Header_jsx.chunk.js:45:22)
