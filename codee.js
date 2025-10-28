c4423d446fff093279b3d8adafcf19ca614aa890
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-28 11:24:35 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -14,3 +14,11 @@ this is the excle sheet data,
 to find out the data wiht filtring ok,
   like coutry or location or more 
 or disply there details 
+
+this is the excle sheet that we upload:: 
+FIRST NAME	TRAVELER 	EMP ID	EMAIL	PNR	LEG TYPE	BEGIN DATE	FROM LOCATION	FROM COUNTRY	END DATE	TO LOCATION	TO COUNTRY
+ROHIT	TRAVELER	326430	rohit.singla@westernunion.com	DSL54P	STOP	10/26/2025 13:20	Hyderabad, Telangana	India	11-01-2025 20.50	Hyderabad, Telangana	India
+JAVIER	TRAVELER		javieralexander.floridolozano@westernunion.com	ZWNTLV	HOTEL	10/28/2025 0:00	Zaragoza, Aragon	Spain	10/30/2025 0:00	Zaragoza, Aragon	Spain
+JORGE	TRAVELER		jorge.gonzalez@westernunion.com	YUPUCM	CAR	10/28/2025 8:30	Barcelona, Catalonia	Spain	10/29/2025 20:00	Barcelona, Catalonia	Spain
+MATIAS	TRAVELER	328263	matias.matta@westernunion.com	JLIAFU	AIR	10/28/2025 5:15	Buenos Aires, Ciudad de Buenos Aires	Argentina	10/28/2025 7:15	Argentina	Argentina
+JESSICA	TRAVELER	303012	jessica.ortizaraya@westernunion.com	WRAAAK	AIR	10/28/2025 19:31	Denver, Colorado	United States	10/28/2025 22:57	Houston, Texas	United States
