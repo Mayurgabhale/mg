f51a1004bd8fd115af7c0cce31e63119f7d60d5d
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-28 15:53:15 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,8 +1,1 @@
-https://lottie.host/2f8b6c6e-bb9a-4cc9-9b29-2e79a3cc1884/1F5iALqUxl.json
-This XML file does not appear to have any style information associated with it. The document tree is shown below.
-<Error>
-<Code>AccessDenied</Code>
-<Message>Access Denied</Message>
-<RequestId>8ZP6S8RR8GV4XY77</RequestId>
-<HostId>nbtTWB0662cNab17Uq4KQQaiAc2NzefRiOU1ZH5pGs2grbWta5h28E6XvVUGBHZGwb5IWL7fuuY4zkoS9obMTE4CLDHxKkG90f+od4p/Kjk=</HostId>
-</Error>
+https://lottiefiles.com/free-animation/loading-40-paperplane-pXSmJB5J2C
