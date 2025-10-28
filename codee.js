553c9fa12a16e65242af2c2cb0920adee0d8088f
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-28 11:20:54 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,10 +1,16 @@
+Read below command and rewrite correct wiht more futuer 
+............
 hi,
   i am staing to create new project:: Travling
 in this: we upload the travaling details excel sheet:
 then read/analize these sheet;
 and this all data diplsy in one page/ dashboard page.
   it is help to compnay to compnay for security resegin.
-security for there employee
+security for there employee and easy to find out ther location,
+  where they traval today, or future trvaling. 
+  AGENCY ID	AGENCY NAME	LAST NAME	FIRST NAME	TRAVELER 	EMP ID	EMAIL	PNR	LEG TYPE	BEGIN DATE	FROM LOCATION	FROM COUNTRY	END DATE	TO LOCATION	TO COUNTRY
+this is the excle sheet data, 
 
-
-हाय, मी नवीन प्रोजेक्ट तयार करण्याचा प्रयत्न करत आहे:: प्रवास यात: आम्ही प्रवासातील तपशील असलेली Excel शीट अपलोड करतो: मग ही शीट वाचतो/विश्लेषण करतो; आणि हा सर्व डेटा एका पृष्ठावर/डॅशबोर्ड पृष्ठावर दर्शवला जातो. हे कंपनीला कामगारांच्या सुरक्षेसाठी मदत करते.
+to find out the data wiht filtring ok,
+  like coutry or location or more 
+or disply there details 
