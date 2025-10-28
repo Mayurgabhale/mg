29e6ccb03b620d75589c9b4ad19c2b4b2e592e50
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-28 11:38:38 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,1 +1,16 @@
-I am starting a new project called Employee Travel Dashboard. In this project, users will be able to upload an Excel sheet containing employee travel details such as agency name, employee ID, email, travel type, dates, and locations. The system will automatically read, analyze, and display this data on a single interactive dashboard page. The dashboard will allow users to filter and search travel information by country, city, employee, date, or other criteria, making it easy to track where employees are currently traveling or plan to travel in the future. This project will help companies enhance employee security, monitor travel activity, and quickly identify travelers’ locations for safety and coordination purposes.
+I am starting a new project called Employee Travel Dashboard. In this project, users will be able to upload an Excel sheet containing employee travel details such as agency name, employee ID, email, travel type, dates, and locations. 
+  The system will automatically read, analyze, and display this data on a single interactive dashboard page. The dashboard will allow users to filter and search travel information by country, city, employee, date, 
+  or other criteria, making it easy to track where employees are currently traveling or plan to travel in the future. 
+  This project will help companies enhance employee security, monitor travel activity, and quickly identify travelers’ locations for safety and coordination purposes.
+
+  AGENCY ID	AGENCY NAME	LAST NAME	FIRST NAME	TRAVELER 	EMP ID	EMAIL	PNR	LEG TYPE	BEGIN DATE	FROM LOCATION	FROM COUNTRY	END DATE	TO LOCATION	TO COUNTRY
+728776	Western Union - Costa Rica	ORTIZ	JESSICA	TRAVELER	303012	jessica.ortizaraya@westernunion.com	WRAAAK	AIR	10/28/2025 19:31	Denver, Colorado	United States	10/28/2025 22:57	Houston, Texas	United States
+728775	Western Union - Argentina	MATTA	MATIAS	TRAVELER	328263	matias.matta@westernunion.com	JLIAFU	AIR	10/28/2025 5:15	Buenos Aires, Ciudad de Buenos Aires	Argentina	10/28/2025 7:15	Argentina	Argentina
+728671	Western Union - United States	WYNNE	ADAM	TRAVELER	326332	adam.wynne@westernunion.com	ACWXUV	CAR	10/27/2025 12:00	Oakland, California	United States	10/29/2025 17:00	Oakland, California	United States
+728778	Western Union - Belgium	GOMEZ GONZALEZ	JORGE	TRAVELER		jorge.gonzalez@westernunion.com	YUPUCM	CAR	10/28/2025 8:30	Barcelona, Catalonia	Spain	10/29/2025 20:00	Barcelona, Catalonia	Spain
+729111	Western Union - Romania	IONESCU	MIHAI	TRAVELER	326540	mihai.ionescu@westernunion.com	YCPJXY	CAR	10/27/2025 5:00	Romania	Romania	11-01-2025 23.00	Romania	Romania
+728776	Western Union - Costa Rica	GARCIA	BRAYAM	TRAVELER	248858	brayam.garcia@westernunion.com	NGCDSQ	HOTEL	10/26/2025 0:00	Singapore City	Singapore	11-01-2025 0.00	Singapore City	Singapore
+729111	Western Union - Romania	IONESCU	MIHAI	TRAVELER	326540	mihai.ionescu@westernunion.com	YCPJXY	HOTEL	10/27/2025 0:00	Oradea	Romania	10/29/2025 0:00	Oradea	Romania
+728775	Western Union - Argentina	CROTTI	ALDANA	TRAVELER	238729	aldana.crotti@westernunion.com	OSESNT	HOTEL	10/27/2025 0:00	AsunciÃ³n	Paraguay	10/30/2025 0:00	AsunciÃ³n	Paraguay
+728766	Western Union - India	SINGLA	ROHIT	TRAVELER	326430	rohit.singla@westernunion.com	DSL54P	STOP	10/26/2025 13:20	Hyderabad, Telangana	India	11-01-2025 20.50	Hyderabad, Telangana	India
+728762	Western Union - Canada	LEGAULT	MARIE-ANDREE	TRAVELER	326779	marieandree.legault@westernunion.com	FXVCGC	STOP	10/27/2025 16:25	Guatemala City	Guatemala	10/31/2025 12:28	Guatemala City	Guatemala
