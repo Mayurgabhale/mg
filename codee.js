1d8f3cc43769bd5ef5a3c4f28b711de8b9ed8bee
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-30 15:56:26 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,19 +1,3 @@
-
-(node:38144) [DEP_WEBPACK_DEV_SERVER_ON_AFTER_SETUP_MIDDLEWARE] DeprecationWarning: 'onAfterSetupMiddleware' option is deprecated. Please use the 'setupMiddlewares' option.
-(Use `node --trace-deprecation ...` to show where the warning was created)
-(node:38144) [DEP_WEBPACK_DEV_SERVER_ON_BEFORE_SETUP_MIDDLEWARE] DeprecationWarning: 'onBeforeSetupMiddleware' option is deprecated. Please use the 'setupMiddlewares' option.
-Starting the development server...
-Compiled successfully!
-
-You can now view client in the browser.
-
-  Local:            http://localhost:3000  this one and
-  Local:            http://localhost:3000/travel-dashboard  like that  
-
-  On Your Network:  http://10.199.32.29:3000
-
-Note that the development build is not optimized.
-To create a production build, use npm run build.
-
-webpack compiled successfully
-can possible to do this 
+{location.pathname !== "/travel-dashboard" && (
+  <ErrorBanner status={connectionStatus} />
+)}
