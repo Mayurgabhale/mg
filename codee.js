c2a83d502b40a89a8b5dbf05f394a686b35e5a51
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-30 15:52:49 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,8 +1,19 @@
-<OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">Travel Dashboard</Tooltip>}>
-  <Nav.Link
-    onClick={() => window.open("/travel-dashboard", "_blank", "noopener,noreferrer")}
-    className="nav-item-infographic"
-  >
-    <i className="fa-solid fa-plane fa-fade"></i>
-  </Nav.Link>
-</OverlayTrigger>
+
+(node:38144) [DEP_WEBPACK_DEV_SERVER_ON_AFTER_SETUP_MIDDLEWARE] DeprecationWarning: 'onAfterSetupMiddleware' option is deprecated. Please use the 'setupMiddlewares' option.
+(Use `node --trace-deprecation ...` to show where the warning was created)
+(node:38144) [DEP_WEBPACK_DEV_SERVER_ON_BEFORE_SETUP_MIDDLEWARE] DeprecationWarning: 'onBeforeSetupMiddleware' option is deprecated. Please use the 'setupMiddlewares' option.
+Starting the development server...
+Compiled successfully!
+
+You can now view client in the browser.
+
+  Local:            http://localhost:3000  this one and
+  Local:            http://localhost:3000/travel-dashboard  like that  
+
+  On Your Network:  http://10.199.32.29:3000
+
+Note that the development build is not optimized.
+To create a production build, use npm run build.
+
+webpack compiled successfully
+can possible to do this 
