886970177015452c4cc5247cfa0ee4f180cb462f
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-10-30 13:22:26 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,4 +1,7 @@
- <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">travel-dashboard</Tooltip>}>
+this page i want ot opne in new window,
+ for security perpose,
+
+<OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">travel-dashboard</Tooltip>}>
               {/* <Nav.Link as={Link} to="/travel-dashboard" className="nav-item-infographic" ><GiAirplaneDeparture  className='nav-item-new'/></Nav.Link> */}
               <Nav.Link as={Link} to="/travel-dashboard" className="nav-item-infographic" ><i class="fa-solid fa-plane fa-fade"></i></Nav.Link>
             </OverlayTrigger>
