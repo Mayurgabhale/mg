95a3753bfcd93570e9313d78b4e7b51b3d0f88a4
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-05 11:15:47 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,5 +1,133 @@
-# snapshot for 2025-11-05 10:40 local server-time for all partitions
-curl "http://localhost:3000/api/occupancy/time-travel?datetime=2025-11-05T10:40:00"
-
-# or for a specific partition (location)
-curl "http://localhost:3000/api/occupancy/time-travel?datetime=2025-11-05T10:40:00&location=UK.London"
+http://localhost:3005/api/occupancy/time-travel?datetime=2025-11-04T23:50:00
+i search this time but i got this time.. 
+  {
+  "success": true,
+  "datetime": "2025-11-04T18:20:00.000Z",
+  "summary": {
+    "total": 399,
+    "Employee": 361,
+    "Contractor": 38
+  },
+  "partitions": {
+    "AUT.Vienna": {
+      "total": 83,
+      "Employee": 73,
+      "Contractor": 10,
+      "floors": {
+        "11th Floor": 83
+      }
+    },
+    "LT.Vilnius": {
+      "total": 177,
+      "Employee": 159,
+      "Contractor": 18,
+      "floors": {
+        "2nd Floor": 8,
+        "9th Floor": 20,
+        "1st Floor": 51,
+        "8th Floor": 15,
+        "10st Floor": 1,
+        "4th Floor": 19,
+        "5th Floor": 17,
+        "10th Floor": 16,
+        "6th Floor": 11,
+        "7th Floor": 15,
+        "3rd Floor": 3,
+        "Basement": 1
+      }
+    },
+    "ES.Madrid": {
+      "total": 31,
+      "Employee": 31,
+      "Contractor": 0,
+      "floors": {
+        "Madrid": 31
+      }
+    },
+    "RU.Moscow": {
+      "total": 18,
+      "Employee": 15,
+      "Contractor": 3,
+      "floors": {
+        "Moscow": 18
+      }
+    },
+    "IT.Rome": {
+      "total": 17,
+      "Employee": 16,
+      "Contractor": 1,
+      "floors": {
+        "Rome": 17
+      }
+    },
+    "IE.Dublin": {
+      "total": 17,
+      "Employee": 13,
+      "Contractor": 4,
+      "floors": {
+        "Dublin": 17
+      }
+    },
+    "MA.Casablanca": {
+      "total": 35,
+      "Employee": 34,
+      "Contractor": 1,
+      "floors": {
+        "7th Floor": 35
+      }
+    },
+    "UK.London": {
+      "total": 5,
+      "Employee": 4,
+      "Contractor": 1,
+      "floors": {
+        "London": 5
+      }
+    },
+    "DU.Abu Dhab": {
+      "total": 16,
+      "Employee": 16,
+      "Contractor": 0,
+      "floors": {
+        "Dubai": 16
+      }
+    }
+  },
+  "details": [
+    {
+      "LocaleMessageTime": "2025-10-20T13:41:36.000Z",
+      "Dateonly": "2025-10-20",
+      "Swipe_Time": "13:41:36",
+      "EmployeeID": "322513",
+      "PersonGUID": "BFEFCB61-3F1B-4B0A-99A6-9091C8EBCF59",
+      "ObjectName1": "Vural, Turkan",
+      "Door": "EMEA_AUT_VIENNA_ICON_11FLR_RECEPTION Door",
+      "PersonnelType": "Employee",
+      "CardNumber": "608903",
+      "Text5": "Vienna-Gertrude",
+      "PartitionName2": "AUT.Vienna",
+      "AdmitCode": "Admit",
+      "Direction": "InDirection",
+      "CompanyName": "WUPSIL Office Austria",
+      "PrimaryLocation": "Vienna-Gertrude",
+      "Floor": "11th Floor"
+    },
+    {
+      "LocaleMessageTime": "2025-10-20T14:26:13.000Z",
+      "Dateonly": "2025-10-20",
+      "Swipe_Time": "14:26:13",
+      "EmployeeID": null,
+      "PersonGUID": "FE7B5E77-9A08-4B6A-A7A1-5EFFE98C7CB8",
+      "ObjectName1": "Temp Card No4, Casablanca",
+      "Door": "EMEA_LTU_VNO_GAMA_2nd floor_West Door",
+      "PersonnelType": null,
+      "CardNumber": "619337",
+      "Text5": null,
+      "PartitionName2": "LT.Vilnius",
+      "AdmitCode": "Admit",
+      "Direction": "InDirection",
+      "CompanyName": null,
+      "PrimaryLocation": null,
+      "Floor": "2nd Floor"
+    },
+    {
