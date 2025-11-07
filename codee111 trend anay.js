e48f998a5397403c3019a70e10520250bb30068c
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-07 15:42:39 +0530

    Update codee111 trend anay.js

--- codee111 trend anay.js
+++ codee111 trend anay.js
@@ -2,3 +2,11 @@ PS C:\Users\W0024618\Desktop\Backend> node checkSheets.js
 Sheets found in Excel:
 [ 'RAW Data Door', 'NEW' ]
 PS C:\Users\W0024618\Desktop
+
+
+
+function readExcel(filePath) {
+  const workbook = XLSX.readFile(filePath);
+  const sheet = workbook.Sheets["NEW"]; // 👈 specify correct sheet name
+  return XLSX.utils.sheet_to_json(sheet, { defval: "" });
+}
