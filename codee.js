e63feed05cea20034921ba05a102a9b32e133589
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-07 11:04:01 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,19 +1,25 @@
+not disply 
+/* === Default (Desktop) === */
 #region-title {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 70px;
-  background-color: #000;
+  background-color: #000000;
   color: #ffdd00;
   border-bottom: 3px solid #ffdd00;
   display: flex;
   align-items: center;
   justify-content: center;
+  z-index: 1000;
+  padding: 24px;
   font-size: 35px;
+  /* border-bottom: 1px solid #000000; */
+  /* border-top: 1px solid #000000; */
   font-family: "PP Right Grotesk";
   font-weight: 600;
-  z-index: 1000;
+
 }
 
 #region-title .region-logo {
@@ -22,16 +28,27 @@
   left: 20px;
 }
 
+
 #region-title .region-logo img {
   height: 40px;
   width: auto;
   display: block;
   cursor: pointer;
-}const logoHTML = `
-    <span class="region-logo">
-        <a href="http://10.199.22.57:3014/">
+}
+
+  const logoHTML = `
+                    <span class="region-logo">
+                       <a href="http://10.199.22.57:3014/">
             <img src="images/home-svgrepo-com.svg" alt="Home"
             style="height: 37px; width: auto; object-fit: contain;" />
         </a>
-    </span>
-`;
+                    </span>
+                    `;
+ <div id="region-title" class="dashboard-header">
+
+        <div class="region-logo">
+            <a href="http://10.199.22.57:3014/">
+                <img src="images/home-svgrepo-com.svg" alt="Home" />
+            </a>
+        </div>
+    </div>
