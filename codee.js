2909d8964fdd0064aab12358d6524fbeb2868790
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-07 11:26:59 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,26 +1,9 @@
-ok disply but i want ot add color 
-i do this but this is not wokr  fill: white;
-
-#region-title .region-logo {
-  position: absolute;
-  top: 10px;
-  left: 20px;
-  z-index: 1000;
-
-}
-
-
-#region-title .region-logo img {
-  height: 40px;
-  width: auto;
-  display: block;
-  cursor: pointer;
-   background-color: rgb(4, 4, 4);
-  fill: white;
-}
-
-<span class="region-logo">
-                      <a href="http://10.199.22.57:3014/">
-                        <img src="images/home-svgrepo-com.svg" alt="Home" />
-                    </a>
+ const logoHTML = `
+                    <span class="region-logo">
+                         <a href="http://10.199.22.57:3014/">
+                            <i class="fa-solid fa-house"></i>
+                         </a>
                     </span>
+                    `;
+
+AFTER HOVER ON THIS ICONS I WANT OT DIPLSY THIS DASHBOARD HUB
