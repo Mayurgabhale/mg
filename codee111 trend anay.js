3ae93db90e386848aed38ddc1ba19b4a6e27250b
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-07 15:41:08 +0530

    Update codee111 trend anay.js

--- codee111 trend anay.js
+++ codee111 trend anay.js
@@ -1,10 +1,4 @@
-const XLSX = require("xlsx");
-const file = "C:\\Users\\W0024618\\Desktop\\Backend\\src\\data\\controllerWithdoor.xlsx";
-
-const workbook = XLSX.readFile(file);
-console.log("Sheets found in Excel:");
-console.log(workbook.SheetNames);
-
-
-
-node checkSheets.js
+PS C:\Users\W0024618\Desktop\Backend> node checkSheets.js
+Sheets found in Excel:
+[ 'RAW Data Door', 'NEW' ]
+PS C:\Users\W0024618\Desktop
