a75efb40fc3b92bbaf6abb396ad90d83de202a69
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-10 11:05:12 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -4,9 +4,19 @@
 >
   <Nav.Link
     href="http://10.199.22.57:3014/"
-    className="nav-item-infographic mr-3"
-    style={{ color: '#FFC107', marginRight: '12px' }}
+    className="nav-item-infographic nav-dashboard-hub"
   >
     <i className="fa-solid fa-house"></i>
   </Nav.Link>
 </OverlayTrigger>
+
+
+
+.nav-dashboard-hub {
+  color: #ffc107 !important;
+  margin-right: 12px;
+}
+
+.nav-dashboard-hub:hover {
+  color: #ffdd33 !important; /* lighter hover color */
+}
