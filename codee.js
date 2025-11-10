0ba5bc66ce611ebfcca16e1671fde8f52c994e16
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-10 13:05:32 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,4 +1,12 @@
-<button
+<div><input accept=".xlsx,.xls,.csv" type="file" style="display: none;"><button disabled="" style="margin-left: 10px; background: rgb(16, 185, 129); color: white; padding: 10px 20px; border: none; border-radius: 8px; cursor: pointer;">Upload File</button></div>
+  {/* <button
+                                    onClick={uploadMonthlySheet}
+                                    style={styles.uploadButton}
+                                    disabled={!monthlyFile}
+                                >
+                                    <FiUpload style={{ marginRight: 8 }} /> Upload File
+                                </button> */}
+                                <button
   onClick={uploadMonthlySheet}
   disabled={!monthlyFile}
   style={{
