7e614a9a684a4b9f37f159ae954f92b4d2e5559a
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-10 11:01:42 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,8 +1,12 @@
- <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-dashboard">Dashboard Hub</Tooltip>}>
-                <Nav.Link href="http://10.199.22.57:3014/" className="nav-item-infographic  mr-3">
-                  <i className="fa-solid fa-house"></i>
-                </Nav.Link> 
-              </OverlayTrigger>
-
-
-how to add color for and margin from right side
+<OverlayTrigger
+  placement="bottom"
+  overlay={<Tooltip id="tooltip-dashboard">Dashboard Hub</Tooltip>}
+>
+  <Nav.Link
+    href="http://10.199.22.57:3014/"
+    className="nav-item-infographic mr-3"
+    style={{ color: '#FFC107', marginRight: '12px' }}
+  >
+    <i className="fa-solid fa-house"></i>
+  </Nav.Link>
+</OverlayTrigger>
