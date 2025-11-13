d2d3482b2c257669352e33c5db588df6fd7ae0e2
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-13 08:54:40 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,51 +1,62 @@
-http://127.0.0.1:8000/daily_sheet/data
+http://127.0.0.1:8000/daily_sheet/regions
 {
-  "summary": {
-    "rows_received": 123,
-    "rows_removed_as_footer_or_empty": 0,
-    "rows_with_parse_errors": 0,
-    "active_now_count": 60,
-    "data_upload_time": "2025-11-13T08:31:09.258703"
-  },
-  "items": [
-    {
-      "index": 0,
-      "agency_id": "728775",
-      "agency_name": "Western Union - Argentina",
-      "first_name": "SANTIAGO",
-      "last_name": "CASTRO",
-      "emp_id": "311107",
-      "email": "santiago.castrofeijoo@wu.com",
-      "pnr": "QDYNNA",
-      "leg_type": "HOTEL",
-      "begin_dt": "2025-11-12T00:00:00+05:30",
-      "end_dt": "2025-11-14T00:00:00+05:30",
-      "from_location": "Buenos Aires, Ciudad de Buenos Aires",
-      "from_country": "Argentina",
-      "to_location": "Buenos Aires, Ciudad de Buenos Aires",
-      "to_country": "Argentina",
-      "active_now": 1,
-      "is_vip": true,
-      "active_now_bool": true
-    },
-    {
-      "index": 1,
-      "agency_id": "728775",
-      "agency_name": "Western Union - Argentina",
-      "first_name": "ESTEBAN",
-      "last_name": "CRESPO",
-      "emp_id": "237072",
-      "email": "esteban.crespo@westernunion.com",
-      "pnr": "DICYVI",
-      "leg_type": "HOTEL",
-      "begin_dt": "2025-11-12T00:00:00+05:30",
-      "end_dt": "2025-11-14T00:00:00+05:30",
-      "from_location": "Buenos Aires, Ciudad de Buenos Aires",
-      "from_country": "Argentina",
-      "to_location": "Buenos Aires, Ciudad de Buenos Aires",
-      "to_country": "Argentina",
-      "active_now": 1,
-      "is_vip": true,
-      "active_now_bool": true
-    },
-    {
+  "regions": {
+    "LACA": {
+      "region_code": "LACA",
+      "total_count": 7,
+      "active_count": 5,
+      "vip_count": 2,
+      "active_vip_count": 2,
+      "cities": {
+        "Buenos Aires": {
+          "city_name": "Buenos Aires",
+          "total_count": 3,
+          "active_count": 3,
+          "vip_count": 2,
+          "active_vip_count": 2,
+          "sample_items": [
+            {
+              "first_name": "SANTIAGO",
+              "last_name": "CASTRO",
+              "email": "santiago.castrofeijoo@wu.com",
+              "pnr": "QDYNNA",
+              "active_now": true,
+              "is_vip": true,
+              "begin_dt": "2025-11-12T00:00:00+05:30",
+              "end_dt": "2025-11-14T00:00:00+05:30"
+            },
+            {
+              "first_name": "ESTEBAN",
+              "last_name": "CRESPO",
+              "email": "esteban.crespo@westernunion.com",
+              "pnr": "DICYVI",
+              "active_now": true,
+              "is_vip": true,
+              "begin_dt": "2025-11-12T00:00:00+05:30",
+              "end_dt": "2025-11-14T00:00:00+05:30"
+            },
+            {
+              "first_name": "DIEGO",
+              "last_name": "LONGO",
+              "email": "diego.longo@westernunion.com",
+              "pnr": "OWNACW",
+              "active_now": true,
+              "is_vip": false,
+              "begin_dt": "2025-11-12T00:00:00+05:30",
+              "end_dt": "2025-11-14T00:00:00+05:30"
+            }
+          ]
+        },
+        "Asunción": {
+          "city_name": "Asunción",
+          "total_count": 3,
+          "active_count": 2,
+          "vip_count": 0,
+          "active_vip_count": 0,
+          "sample_items": [
+            {
+              "first_name": "DENISE",
+              "last_name": "GOLDSTEIN",
+              "email": "denise.goldstein@wu.com",
+              "pnr": "KXZKWW",
+              "active_now": true,
