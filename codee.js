d54d085793ad6ddb9105cf700cff88955d34b0c3
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-18 10:47:02 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,3 +1,17 @@
+
+
+document.getElementById("toggle-main-btn").addEventListener("click", function () {
+    const mainContent = document.getElementById("content");
+
+    if (mainContent.style.display === "none") {
+        mainContent.style.display = "block";   // Show
+    } else {
+        mainContent.style.display = "none";    // Hide
+    }
+});
+
+
+
 <button class="nav-button" id="toggle-main-btn">
   <i class="fas fa-window-maximize"></i> Show / Hide Device Details
 </button>
