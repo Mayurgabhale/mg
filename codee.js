bf09fcd674adebf6864493ed5dadc246b4d5f78b
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-21 11:11:36 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,14 +1,11 @@
-??' and '||' operations cannot be mixed without parentheses.ts(5076)
-any
-
 plugins: {
-        legend: { display: false },
-        tooltip: {
-          callbacks: {
-            label: function (ctx) {
-              return `${ctx.label} : ${ctx .parsed.y ?? ctx.parsed || 0}`;
-            }
-          }
-        }
+  legend: { display: false },
+  tooltip: {
+    callbacks: {
+      label: function (ctx) {
+        const value = ctx.parsed?.y ?? 0;
+        return `${ctx.label} : ${value}`;
       }
-    },
+    }
+  }
+}
