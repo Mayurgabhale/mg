8372598afb6eb117521edba09bc9802e8f3631db
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-28 11:27:11 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,13 +1,1 @@
-function renderOfflineChartFromCombined(combinedDevices) {
-  const offlineDevices = combinedDevices
-    .filter(d => d.device.status === "offline")
-    .map(d => ({
-      device: d.device,
-      type: d.device.type
-    }));
-
-  updateOfflineChart(offlineDevices);
-
-  // ✅ ADD BAR CHART UPDATE HERE
-  updateOfflineCityBarChart(combinedDevices);
-}
+renderOfflineChartFromCombined(combinedDevices);
