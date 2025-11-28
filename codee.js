68a31d9408f005f1b3f41aa77ed1cef651154f2b
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-28 15:07:30 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -8,3 +8,13 @@ const deviceObjects = combinedDevices.map(item => item.device);
 const offlineCityData = buildOfflineCityCount(combinedDevices);
 
 console.log("Offline city data:", offlineCityData);
+
+
+...
+function convertOfflineCityForMap(offlineCities) {
+    return offlineCities.map(city => ({
+        city: city.city,
+        offlineDevices: city.offline,
+        totalOffline: Object.values(city.offline).reduce((a, b) => a + b, 0)
+    }));
+}
