63efb6d24f9981a189b71de0285d64132e84d2d3
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-29 08:09:57 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,6 +1,1 @@
-if (short && result[city].counts.hasOwnProperty(short)) {
-    result[city].counts[short]++;
-    if (status === "offline") {
-        result[city].offline[short] = (result[city].offline[short] || 0) + 1;
-    }
-}
+offline: barDetail ? barDetail.offline : tt.offline
