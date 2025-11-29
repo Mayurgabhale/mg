7e8044589e4aa66d8a760404d488181e2af2e8e6
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-11-29 07:53:56 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,3 +1,1 @@
-if (status === "offline") {
-    result[city].offlineCount = (result[city].offlineCount || 0) + 1;
-}
+if (status === "offline") result[city].offlineCount = (result[city].offlineCount || 0) + 1;
