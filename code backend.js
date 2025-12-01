1fb4ec30733d5cc71de932faa9e8607a53a52d66
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-01 13:25:15 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -1,14 +1,18 @@
-import os
-import platform
-import subprocess
-
-def ping(ip):
-    """
-    Ping a device, return 'Online' or 'Offline'
-    """
-    param = "-n" if platform.system().lower()=="windows" else "-c"
-    try:
-        output = subprocess.check_output(["ping", param, "1", ip], stderr=subprocess.DEVNULL)
-        return "Online"
-    except subprocess.CalledProcessError:
-        return "Offline"
+how to save device in database 
+C:\Users\W0024618\Desktop\Backend\src\data
+├───data
+│       ArchiverData.xlsx
+│       archivers.json
+│       CameraData.xlsx
+│       cameras.json
+│       ControllerData.xlsx
+│       ControllerDataWithDoorReader.json
+│       controllers.json
+│       controllerWithdoor.xlsx
+│       DBDetails.json
+│       DBDetails.xlsx
+│       pcDetails.json
+│       PCDetails.xlsx
+│       ServerData.xlsx
+│       servers.json
+│
