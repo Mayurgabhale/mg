29f01088a38e1fccacca1d4dff1f3b510b581e28
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-01 11:55:16 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -1,140 +1,16 @@
-yes we got this 
-http://localhost/api/devices/all
+http://localhost/api/devices
+
+
+
 {
-  "summary": {
-    "totalDevices": 301,
-    "totalOnlineDevices": 251,
-    "totalOfflineDevices": 50,
-    "archivers": {
-      "total": 21,
-      "online": 21,
-      "offline": 0
-    },
-    "controllers": {
-      "total": 71,
-      "online": 69,
-      "offline": 2
-    },
-    "cameras": {
-      "total": 177,
-      "online": 133,
-      "offline": 44
-    },
-    "servers": {
-      "total": 5,
-      "online": 5,
-      "offline": 0
-    },
-    "pcDetails": {
-      "total": 17,
-      "online": 13,
-      "offline": 4
-    },
-    "DBDetails": {
-      "total": 10,
-      "online": 10,
-      "offline": 0
-    }
-  },
-  "details": {
-    "archivers": [
-      {
-        "archivername": "Archiver Manila",
-        "ip_address": "10.193.132.8",
-        "location": "APAC",
-        "city": "Taguig City",
-        "history": [],
-        "status": "Online"
-      },
-      {
-        "archivername": "Archiver Taguig City Philippines",
-        "ip_address": "10.194.2.190",
-        "location": "APAC",
-        "city": "Taguig City",
-           }
-    ],
-    "controllers": [
-      {
-        "controllername": "IN-PUN-2NDFLR-ISTAR PRO",
-        "ip_address": "10.199.13.10",
-        "location": "APAC",
-        "city": "Pune 2nd Floor",
-        "history": [],
-        "status": "Online"
-      },
-      {
-        "controllername": "IN-PUN-PODIUM-ISTAR PRO-01",
-        "ip_address": "10.199.8.20",
-        "location": "APAC",
-        "city": "Pune Podium",
-        "history": [],
-        "status": "Online"
-           ],
-    "cameras": [
-      {
-        "cameraname": "Green Zone to yellow zone passage - 10.199.10.20 (FLIR CM-3102-11-I T71552107) - 350",
-        "ip_address": "10.199.10.20",
-        "location": "APAC",
-        "city": "Pune Podium",
-        "deviec_details": "FLIR",
-        "history": [],
-        "status": "Online"
-      },
-      {
-        "cameraname": "IN-PNQ-PF - Green Zone Exit Passage 10.199.10.139",
-        "ip_address": "10.199.10.139",
-        "location": "APAC",
-        "city": "Pune Podium",
-        "deviec_details": "FLIR",
-        "history": [],
-        "status": "Online"
-      },
-      {
-        "cameraname": "IN-PNQ-PF - Green Zone IDF (01) Room
-            "servers": [
-      {
-        "servername": "Master Server ",
-        "ip_address": "10.58.118.20",
-        "location": "NAMER",
-        "city": "Denver Colorado",
-        "history": [],
-        "status": "Online"
-      },
-      {
-        "servername": "NAMER Server ",
-        "ip_address": "10.58.118.21",
-         "pcDetails": [
-      {
-        "hostname": "WKSWUPUN4501",
-        "ip_address": "WKSWUPUN4501",
-        "location": "APAC",
-        "city": "Pune Podium",
-        "pc_name": "Screen 03",
-        "history": [],
-        "status": "Online"
-      },
-      {
-        "hostname": "WKSPUN-392353",
-        "ip_address": "WKSPUN-392353",
-        "location": "APAC",
-        "city": "Pune Podium",
-        "pc_name": "Screen 04",
-        "history": [],
-           "DBDetails": [
-      {
-        "location": "NAMER",
-        "city": "Denver",
-        "hostname": "SRVWUDEN0890v",
-        "ip_address": "10.58.118.22",
-        "application": "CCURE MAS SQL DB",
-        "windows_server": "Windows Server 2019 Standard",
-        "history": [],
-        "status": "Online"
-      },
-      {
-        "location": "NAMER",
-        "city": "Denver",
-        "hostname": "SRVWUDEN0190V",
-        "ip_address": "10.58.118.20",
-        "application": "CCURE MAS APP",
-        "windows_server": "Windows Server 2016 Standar
+  "type": "cameras",
+  "device": {
+    "cameraname": "HYD_2FLR_B WING PASSAGE CAM 1 - 10.200.3.41-136",
+    "ip_address": "10.200.3.41",
+    "location": "APAC",
+    "city": "HYDERABAD",
+    "deviec_details": "axis",
+    "hyperlink": "http://10.200.3.41/camera/index.html",
+    "remark": ""
+  }
+}
