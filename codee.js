0a5af7b00bf89a74ca3fbda4576cc9d172408e2f
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-01 19:21:45 +0530

    Update codee.js

--- codee.js
+++ codee.js
@@ -1,33 +1,5 @@
-<div style={{ display:'flex', justifyContent:'space-between', alignItems:'center', marginTop:8 }}>
-  <div style={{ fontSize:13 }}>
-    {`Showing ${start+1} - ${Math.min(end, filteredAll.length)} of ${filteredAll.length}`}
-  </div>
+const start = detailsOffset;
+const end = detailsOffset + detailsLimit;
 
-  <div style={{ display:'flex', gap:8 }}>
-    {/* PREV */}
-    <button
-      className="small-button"
-      onClick={() => {
-        const newOffset = Math.max(0, detailsOffset - detailsLimit);
-        setDetailsOffset(newOffset);
-      }}
-      disabled={detailsOffset <= 0}
-    >
-      Prev
-    </button>
 
-    {/* NEXT */}
-    <button
-      className="small-button"
-      onClick={() => {
-        const newOffset = detailsOffset + detailsLimit;
-        if (newOffset < filteredAll.length) {
-          setDetailsOffset(newOffset);
-        }
-      }}
-      disabled={detailsOffset + detailsLimit >= filteredAll.length}
-    >
-      Next
-    </button>
-  </div>
-</div>
+const paginatedRows = filteredAll.slice(start, end);
