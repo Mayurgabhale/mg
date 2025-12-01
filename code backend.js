a74bb94f330ed2a0b7a15f4c1712afe2275136d4
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-01 13:30:54 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -1,3 +1,65 @@
+[notice] A new release of pip is available: 25.1.1 -> 25.3
+[notice] To update, run: python.exe -m pip install --upgrade pip
+(venv) PS C:\Users\W0024618\Desktop\Backend\python-service> python import_devices.py               
+Traceback (most recent call last):
+  File "C:\Users\W0024618\Desktop\Backend\python-service\import_devices.py", line 48, in <module>
+    save_excel_to_db(os.path.join(DATA_DIR, "CameraData.xlsx"), "camera")
+    ~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\W0024618\Desktop\Backend\python-service\import_devices.py", line 12, in save_excel_to_db
+    df = pd.read_excel(file_path)
+  File "C:\Users\W0024618\Desktop\Backend\python-service\venv\Lib\site-packages\pandas\io\excel\_base.py", line 495, in read_excel
+    io = ExcelFile(
+        io,
+    ...<2 lines>...
+        engine_kwargs=engine_kwargs,
+    )
+  File "C:\Users\W0024618\Desktop\Backend\python-service\venv\Lib\site-packages\pandas\io\excel\_base.py", line 1550, in __init__
+    ext = inspect_excel_format(
+        content_or_path=path_or_buffer, storage_options=storage_options
+    )
+  File "C:\Users\W0024618\Desktop\Backend\python-service\venv\Lib\site-packages\pandas\io\excel\_base.py", line 1402, in inspect_excel_format
+    with get_handle(
+         ~~~~~~~~~~^
+        content_or_path, "rb", storage_options=storage_options, is_text=False
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ) as handle:
+    ^
+  File "C:\Users\W0024618\Desktop\Backend\python-service\venv\Lib\site-packages\pandas\io\common.py", line 882, in get_handle
+    handle = open(handle, ioargs.mode)
+FileNotFoundError: [Errno 2] No such file or directory: 'C:\\Users\\W0024618\\Desktop\\Backend\\python-service\\data\\CameraData.xlsx'
+(venv) PS C:\Users\W0024618\Desktop\Backend\python-service> 
+
+########
+    C:\Users\W0024618\Desktop\Backend\src\data\CameraData.xlsx
+    C:\Users\W0024618\Desktop\Backend\src\data\cameras.json
+PS C:\Users\W0024618\Desktop\Backend\src\data> dir
+
+
+    Directory: C:\Users\W0024618\Desktop\Backend\src\data
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a----         7/31/2025   9:53 PM          17446 ArchiverData.xlsx
+-a----         12/1/2025  12:39 PM           3809 archivers.json
+-a----         12/1/2025  10:30 AM        3400706 CameraData.xlsx
+-a----         12/1/2025  12:39 PM          41143 cameras.json
+-a----         11/7/2025   3:24 PM          13383 ControllerData.xlsx
+-a----         11/7/2025   3:54 PM         103000 ControllerDataWithDoorReader.json
+-a----         12/1/2025  12:39 PM          13219 controllers.json
+-a----         11/7/2025   3:06 PM          54171 controllerWithdoor.xlsx
+-a----         12/1/2025  12:39 PM           2604 DBDetails.json
+-a----         8/25/2025   1:13 PM          11071 DBDetails.xlsx
+-a----         12/1/2025  12:39 PM           3432 pcDetails.json
+-a----         8/26/2025   3:49 PM          11131 PCDetails.xlsx
+-a----         8/25/2025   4:10 PM          10730 ServerData.xlsx
+-a----         12/1/2025  12:39 PM            855 servers.json
+
+
+PS C:\Users\W0024618\Desktop\Backend\src\data> 
+
+    see this 
+    
 import json
 import pandas as pd
 from database import get_db
@@ -41,7 +103,7 @@ def save_excel_to_db(file_path, device_type, doors_json=None):
     print(f"Saved {device_type} data from {os.path.basename(file_path)}")
 
 
-pip install openpyxl pandas
+
 
 
 # Example: load all Excel files
