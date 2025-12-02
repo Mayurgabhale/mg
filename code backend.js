0578e8a8c0d752edfb9bcb22d1b4c81e6dd93b9b
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-02 16:16:09 +0530

    Update code backend.js travel incident reporting form

--- code backend.js
+++ code backend.js
@@ -1,78 +1,63 @@
-this is my old if, in this need to do any changeses
+Incident Reporting Form
+When you submit this form, it will not automatically collect your details like name and email address unless you provide it yourself.
+*Required
+1.Type of Incident / Accident 
+•	Medical 
+•	Theft 
+•	Fire 
+•	HR Related Incident 
+•	Outside Work Place Violence 
+•	Threat 
+•	Death 
+•	Fraud 
+•	Any Other Safety / Security Related Incident 
+•	Other – Enter your answer
+2.Date of Report 
+3.Time of Report (HH:MM)
+4.Name of Impacted Employee / Person
+5.Employee ID of Impacted Employee
+6.Was this incident reported verbally before submitting this report?
+ ** In case of medical emergency inform local HR
+•	Yes
+•	No
 
-C:\Users\W0024618\Desktop\Backend\src\controllers\regionControllers.js
-const { fetchGlobalData, fetchRegionData} = require("../services/excelService");
-
-
-// Global Summary Controller
-const getGlobalSummary = async (req, res) => {
-    try {
-        const globalData = await fetchGlobalData();
-        res.status(200).json({ summary: globalData.summary });
-    } catch (error) {
-        res.status(500).json({ error: "Internal server error", details: error.message });
-    }
-};
-
-// Global Details Controller
-const getGlobalDetails = async (req, res) => {
-    try {
-        const globalData = await fetchGlobalData();
-        res.status(200).json({ details: globalData.details });
-    } catch (error) {
-        res.status(500).json({ error: "Internal server error", details: error.message });
-    }
-};
-
-// Region Summary Controller
-const getRegionSummary = async (req, res) => {
-    const { regionName } = req.params;
-    try {
-        const regionData = await fetchRegionData(regionName);
-        res.status(200).json({ summary: regionData.summary });
-    } catch (error) {
-        res.status(500).json({ error: "Internal server error", details: error.message });
-    }
-};
-
-
-// Region Details Controller
-const getRegionDetails = async (req, res) => {
-    const { regionName } = req.params;
-    try {
-        const regionData = await fetchRegionData(regionName);
-        res.status(200).json({ details: regionData.details });
-    } catch (error) {
-        res.status(500).json({ error: "Internal server error", details: error.message });
-    }
-};
-
-module.exports = {
-    getGlobalSummary,
-    getGlobalDetails,
-    getRegionSummary,
-    getRegionDetails,
-};
+If Yes then
+7.Incident reported to: 
+•	Supervisor
+•	Manager
+•	HR
+•	Other Employee
+•	Not Reported
+8.If Yes, to whom ( Name and Department ):
+9.Location of Incident or Accident (Specify Office / Branch)
+10.Reported By - Name:
+11.Reported By - Employee ID #
+12.Reported By - Contact Number
+13.Date of Incident Occurred
+14.Time of Incident 
+15.Detailed Description of Incident (long ans)
+16.Immediate Actions Taken: (long ans)
+17.Accompanying Person Name and Contact Details
+18.Name of Witnesses or Employee present during the incident/accident:
+19.Contact Number - Witness or Employee
+20.Root cause analysis of the incident/accident:
+21.Preventive actions taken during or after incident/accident (If any):
+Submit 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+If No then 
+7.Location of Incident or Accident (Specify Office / Branch)
+8.Reported By - Name:
+9.Reported By - Employee ID #
+10.Reported By - Contact Number
+11.Date of Incident Occurred
+12.Time of Incident 
+13.Detailed Description of Incident  (long ans)
+14.Immediate Actions Taken: (long ans)
+15.Accompanying Person Name and Contact Details
+16.Name of Witnesses or Employee present during the incident/accident:
+17.Contact Number - Witness or Employee
+18.Root cause analysis of the incident/accident:
+19.Preventive actions taken during or after incident/accident (If any):
 
 
