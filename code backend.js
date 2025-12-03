e320645c2776a36fc141896acb249aba99b89e33
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-03 11:05:51 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -1,2 +1,4 @@
-C:\Users\W0024618\Desktop\IncidentDashboard\Frontend
-ok in this 
+cd C:\Users\W0024618\Desktop\IncidentDashboard
+npx create-react-app Frontend
+cd Frontend
+npm install react-router-dom react-icons axios
