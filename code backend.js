5bf734e60fd0556ede6190479d972c1d5629ea2d
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-04 15:44:16 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -1,3 +1,27 @@
+// Delete device
+function deleteDevice(ip) {
+  const found = findInAllData(ip);
+  if (!found) throw new Error("Device not found");
+  const { listName, idx } = found;
+
+  const tableMap = {
+    archivers: "archivers",
+    controllers: "controllers",
+    cameras: "cameras",
+    servers: "servers",
+    pcDetails: "pc_details",
+    DBDetails: "dbdetails",
+  };
+  const table = tableMap[listName];
+  if (!table) throw new Error("Unsupported device type for delete: " + listName);
+
+  db.prepare(`DELETE FROM ${table} WHERE ip_address = ?`).run(ip);
+  allData[listName].splice(idx, 1);
+  rebuildIpRegionMap();
+  return true;
+}
+above is my 
+-----
 function deleteDevice(type, ip) {
   const table = mapTypeToTable(type);
 
