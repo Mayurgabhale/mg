16e6422de7fefca2e698f405c15f91cb8f17c0df
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-05 10:27:30 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -637,28 +637,7 @@ function updateDevice(oldIp, updateFields) {
   return allData[listName][idx];
 }
 
-// Delete device
-// function deleteDevice(ip) {
-//   const found = findInAllData(ip);
-//   if (!found) throw new Error("Device not found");
-//   const { listName, idx } = found;
 
-//   const tableMap = {
-//     archivers: "archivers",
-//     controllers: "controllers",
-//     cameras: "cameras",
-//     servers: "servers",
-//     pcDetails: "pc_details",
-//     DBDetails: "dbdetails",
-//   };
-//   const table = tableMap[listName];
-//   if (!table) throw new Error("Unsupported device type for delete: " + listName);
-
-//   db.prepare(`DELETE FROM ${table} WHERE ip_address = ?`).run(ip);
-//   allData[listName].splice(idx, 1);
-//   rebuildIpRegionMap();
-//   return true;
-// }
 // ⬇️⬇️
 // Delete device
 function deleteDevice(ip) {
