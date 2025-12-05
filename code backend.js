9b4ecd3ba3d4c6c3a1d007b039074484d0c23c02
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-05 12:36:02 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -6,3 +6,19 @@ if (mode === "add") {
     added.value = currentUserName;      // fill with logged-in user
     added.readOnly = false;             // editable in ADD mode
 }
+
+
+...
+if (mode === "edit") {
+    document.getElementById("added-by-box").style.display = "block";
+    document.getElementById("updated-by-box").style.display = "block";
+
+    const added = document.getElementById("device-added-by");
+    const updated = document.getElementById("device-updated-by");
+
+    added.value = deviceObj.added_by || "";
+    added.readOnly = true;              // read-only in EDIT mode
+
+    updated.value = currentUserName;    // editable
+    updated.readOnly = false;
+}
