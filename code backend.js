3b91d6b8d771761cad687e38d50b7cc97b37f5d2
Mayurgabhale <138611577+Mayurgabhale@users.noreply.github.com>
2025-12-11 11:26:12 +0530

    Update code backend.js

--- code backend.js
+++ code backend.js
@@ -1,12 +1,12 @@
-<Container
-  maxWidth={false}
-  disableGutters
-  sx={{
-    py: 1,
-    px: { xs: 1, sm: 2, md: 3 },
-    width: '100vw',
-    minHeight: '100vh',
-    background: 'rgba(0,0,0,0.6)',
-    boxSizing: 'border-box'
-  }}
->
+Uncaught runtime errors:
+×
+ERROR
+Element type is invalid. Received a promise that resolves to: [object Object]. Lazy element type must resolve to a class or function.
+    at beginWork (http://localhost:3000/static/js/bundle.js:20794:17)
+    at runWithFiberInDEV (http://localhost:3000/static/js/bundle.js:15316:68)
+    at performUnitOfWork (http://localhost:3000/static/js/bundle.js:23225:93)
+    at workLoopSync (http://localhost:3000/static/js/bundle.js:23120:38)
+    at renderRootSync (http://localhost:3000/static/js/bundle.js:23105:7)
+    at performWorkOnRoot (http://localhost:3000/static/js/bundle.js:22778:33)
+    at performWorkOnRootViaSchedulerTask (http://localhost:3000/static/js/bundle.js:23880:5)
+    at MessagePort.performWorkUntilDeadline (http://localhost:3000/static/js/bundle.js:42134:46)
